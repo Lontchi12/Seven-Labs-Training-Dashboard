ad8bfbe282e1afed4d937526979c2af47ab4c582
Lontchi12 <lontchilionelle@gmail.com>
2021-07-05 16:40:32 +0100

    resolved conflicts in Auth.ts file

--- src/app/frontend/models/Auth.ts
+++ src/app/frontend/models/Auth.ts
@@ -4,9 +4,5 @@ export interface Auth{
     id?:number,
     username: string,
     password: string,
-<<<<<<< HEAD
     role?: Role
-=======
-    
->>>>>>> faad67fbf84bb9da101802c416329b10e84436d0
 }

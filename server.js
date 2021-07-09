7d4653d9f3f98a86e8a840f9e2d616688b8a134f
traore19 <traorefadimatou19@gmail.com>
2021-07-09 11:14:07 +0100

    configurations made

--- server.js
+++ server.js
@@ -4,13 +4,24 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/seven-labs-frontend'));
+// import expose from './expose.js';
 
-app.get('/*', function(req,res) {
+// Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist/index.html'));
+
+// app.get('*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/seven-labs-frontend/index.html'));
+// res.sendFile(path.join(__dirname+'/dist/index.html'));
+// });
+
+app.use(express.static(path.join(__dirname, '/dist/')));
+
+app.get('*', (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname,  'dist', 'index.html')
+  );
 });
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);

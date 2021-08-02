21371d8efc4accc9904bb93766a393fbef2fd984
Lontchi12 <lontchilionelle@gmail.com>
2021-08-02 15:54:23 +0100

    edited server.js

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ app.use(compression())
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/index.html'));
 
+// res.header('Content-Security-Policy', 'img-src 'self'');
+
+const csp = require('express-csp-header');
+app.use(csp({
+    policies: {
+        'default-src': [csp.NONE],
+        'img-src': [csp.SELF],
+    }
+}));
+
 // app.get('*', function(req,res) {
     
 // res.sendFile(path.join(__dirname+'/dist/index.html'));
@@ -27,3 +37,6 @@ app.get('*', (req, res) => {
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+
+// "build": "ng build --watch --configuration production ",

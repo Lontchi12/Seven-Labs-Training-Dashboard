19671289bb505dcb4a4ce4ead2d9b740d5b59238
Lontchi12 <lontchilionelle@gmail.com>
2021-08-02 15:22:09 +0100

    edited server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, '/dist/')));
 
 app.get('*', (req, res) => {
   res.sendFile(
-    path.resolve(__dirname,  'dist', 'index.html')
+    path.resolve(__dirname+'/dist/index.html')
   );
 });
 
